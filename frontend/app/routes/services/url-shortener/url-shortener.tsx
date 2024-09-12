0484d4ce12899a35f31b71dada0a801f9ffac362
NickooMar <nicoo.marsili@gmail.com>
2024-09-11 22:20:36 -0300

    fix: oxlint

---
+++ frontend/app/routes/services/url-shortener/url-shortener.tsx
@@ -0,0 +1,7 @@
+const URLShortener: React.FC = () => {
+    return (
+        <></>
+    )
+}
+
+export default URLShortener
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,8 +4,6 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 const svgToDataUri = require("mini-svg-data-uri");
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
 
 const config = {
   darkMode: ["class"],
